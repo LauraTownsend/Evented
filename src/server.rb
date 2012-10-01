962cdb4a3baa127caf0c916615ffbda95f566c1e
LauraMcCormack <laura.mccormack227@gmail.com>
2012-10-01 20:19:44 +0100

    comment header added to server.rb

--- src/server.rb
+++ src/server.rb
@@ -1,3 +1,13 @@
+# University Events Web Application
+#
+# Ruby RESTful server
+# Dissertation Project 2012-13
+# 
+# Implementing a server in Ruby using the principles of REST
+# The server allows applications to manage/book university events
+#
+# author: Laura McCormack
+
 require 'sinatra'
 require 'haml'
 
