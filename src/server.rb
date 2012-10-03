4e75f053399aeaef518dc651a040a02b69e72a94
LauraMcCormack <laura.mccormack227@gmail.com>
2012-10-03 11:44:01 +0100

    http request handling outline in server.rb

--- src/server.rb
+++ src/server.rb
@@ -15,4 +15,60 @@ get '/' do
 	:haml index
 end
 
+# handle http GET request on customer
+get '/cust' do
 
+end
+
+# handle http POST request on customer
+post '/cust' do
+
+end
+
+# handle http PUT request on customer
+put '/cust' do
+
+end
+
+# handle http DELETE request on customer
+delete '/cust' do
+
+end
+
+# handle http GET request on event
+get '/event' do
+
+end
+
+# handle http POST request on event
+post '/event' do
+
+end
+
+#handle http PUT request on event
+put '/event' do
+
+end
+
+# handle http DELETE request on event
+delete '/event' do
+
+end
+
+# handle http GET request on staff
+get '/staff' do
+end
+
+# handle http POST request on staff
+post '/staff' do
+end
+
+# handle http PUT request on staff
+put '/staff' do
+
+end
+
+# handle http DELETE request on staff
+delete '/staff' do
+
+end
